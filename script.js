b44274e3e98d00cf661d11e63a19d6d87ffe7fe9
Aastha <aasthabisht2345@gmail.com>
2023-10-19 18:26:13 +0530

    added html, css

---
+++ script.js
@@ -0,0 +1,16 @@
+const app = document.querySelector('.app'),
+    mode = document.querySelector('#mode'),
+
+    chats = document.querySelector('.chats'),
+    add_chat=document.querySelector('#add-chat'),
+
+    clear = document.querySelector('#delete'),
+
+    result = document.querySelector('.qna'),
+
+    input = document.querySelector('.input-group input'),
+
+    send = document.querySelector('#send');
+
+
+
